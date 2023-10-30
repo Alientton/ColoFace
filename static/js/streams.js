14a5317d1e7957f5ef2da658ed431994897a0fd1
Alientton <yanganton024@gmail.com>
2023-10-29 23:21:12 -0400

    completed handleUserLeft function

--- static/js/streams.js
+++ static/js/streams.js
@@ -9,6 +9,10 @@ let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
+    client.on('user-published', handleUserJoined)
+    client.on('user-left', handleUserLeft)
+
+
     UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
     // get user's audio and video track
@@ -27,6 +31,39 @@ let joinAndDisplayLocalStream = async () => {
 
     // publish the track
     await client.publish([localTracks[0], localTracks[1]])
+}
+
+let handleUserJoined = async (user, mediaType) => {
+    // add user to the remoteUsers array
+    remoteUsers[user.uid] = user
+    await client.subscribe(user, mediaType)
+
+    if(mediaType === 'video'){
+        // make sure the user's video player doesn't already exist within our dom
+        let player = document.getElementById(`user-contianer-${user.uid}`)
+        if(player != null){
+            player.remove()
+        }
+
+        player = `<div class="video-container" id="user-container-${user.uid}">
+                <div class="username-wrapper"><span class="user-name">My Name</span></div>
+                <div class="video-player" id="user-${user.uid}"></div>
+                </div>`
+        //It takes an HTML element with the id 'video-streams' and 
+        // appends the HTML content stored in the player variable to the end of that element.
+        document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+        user.videoTrack.play(`user-${user.uid}`)
+
+    }
+
+    if (mediaType === 'audio'){
+        user.audioTrack.play()
+    }
+}
+
+let handleUserLeft = async(user) => {
+    delete remoteUsers[user.uid]
+    document.getElementById(`user-container-${user.uid}`).remove()
 }
 
 joinAndDisplayLocalStream()
